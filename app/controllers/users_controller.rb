544f45b8c1b0e8185d75c807980010587ca6c940
Sarthak333 <sarthak333jha@gmail.com>
2019-01-14 16:30:19 +0530

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,17 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
   end
   def create
-  @user= User.new(user_params)
+  @user = User.new(user_params)
   if @user.save
+    log_in @user
     flash[:success] = "Welcome to the Sample App!"
-  redirect_to @user
-else
-  render 'new'
-    end
+    redirect_to @user
+  else
+    render 'new'
   end
+end
+
+
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
