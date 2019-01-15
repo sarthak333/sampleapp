b3c56dcc590e832ef8dc3f61a0d45c65a2c2f700
Sarthak333 <sarthak333jha@gmail.com>
2019-01-15 23:21:12 +0530

    ftry2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.raise_delivery_errors = true
    config.action_mailer.delivery_method = :test
-   host = 'localhost:3000'                     # Local server
-   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+   
 end
