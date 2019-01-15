f2f5c36f4f49234b47364828d6a5d56e48c01dbe
Sarthak333 <sarthak333jha@gmail.com>
2019-01-15 23:32:59 +0530

    ftry4

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,6 +42,7 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   config.action_mailer.delivery_method = :test
+  
   config.action_mailer.default_url_options = { host: 'salty-plains-80839.herokuapp.com' }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
