4e822bac94e5ef98ec54c1c66d23f7afba9308cd
Sarthak333 <sarthak333jha@gmail.com>
2019-01-16 12:42:24 +0530

    new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'guarded-fjord-67926.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host, protocol: https }
+  config.action_mailer.default_url_options = { host: host}
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
