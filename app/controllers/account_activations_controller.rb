5608c6fd356f52394c346ab9d2383cbae684b2c2
Sarthak333 <sarthak333jha@gmail.com>
2019-01-16 13:29:25 +0530

    finaltry

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,8 +3,8 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      update_attribute(:activated,    true)
-      update_attribute(:activated_at, Time.zone.now)
+      user.update_attribute(:activated,    true)
+     user.update_attribute(:activated_at, Time.zone.now)
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
